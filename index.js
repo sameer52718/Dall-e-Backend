16d708557a194ef85f123925a532a4092d725f62
Sameer Ali <alisameer52718@gmail.com>
2023-01-27 16:09:52 +0500

    ready for deployment

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDb from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({limit:'50mb'}));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/' ,async ( req , res ) => {
+    res.send('Hello From Sameer')
+});
+
+const startServer = async () => {
+
+    try {
+        connectDb(process.env.MONGODB_URL)
+        app.listen(8080 , () => console.log('server is started on port 8080 http://localhost:8080'))
+    } catch (error) {
+        console.log(error)
+    }
+
+
+}
+
+startServer();
